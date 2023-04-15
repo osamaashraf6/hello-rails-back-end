eafc84a6139d7c5e3db87b5b6e6f710f29c83c28
osamaashraf6 <osamaashraf668@gmail.com>
2023-04-15 10:38:57 +0200

    Add the routes for the project and configure the database.yml for the pg database

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+end
